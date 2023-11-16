430bee8f8d66bb5acb387a6999df8f28ff0030f0
ArjunG12 <46287498+ArjunG12@users.noreply.github.com>
2023-11-16 10:33:18 +0530

    Create rasp_2_arduino.py

---
+++ robot_movement/rasp_2_arduino.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String, Float64
+import time
+import serial
+
+# Serial port configuration
+SERIAL_PORT = '/dev/ttyACM0'  # Replace with the correct port for your Arduino
+BAUD_RATE = 9600
+
+# ROS node initialization
+rospy.init_node('motor_velocity_publisher')
+
+# Serial port initialization
+serial_port = serial.Serial(SERIAL_PORT, BAUD_RATE)
+
+# Variables to store left and right velocities
+left_velocity = 0.0
+right_velocity = 0.0
+
+# ROS message callback function for left motor
+def left_velocity_callback(data):
+    global left_velocity
+    left_velocity = data.data
+
+# ROS message callback function for right motor
+def right_velocity_callback(data):
+    global right_velocity
+    right_velocity = data.data
+
+# ROS publisher initialization
+pub = rospy.Publisher('velocity_string', String, queue_size=10)
+
+# ROS subscriber initialization
+rospy.Subscriber('left_motor_velocity', Float64, left_velocity_callback)
+rospy.Subscriber('right_motor_velocity', Float64, right_velocity_callback)
+
+# ROS spin
+rate = rospy.Rate(10)  # Adjust the rate as per your requirement
+velocity_string=b''
+while not rospy.is_shutdown():
+    if left_velocity > 0:
+        velocity_string+=b'L'
+    elif left_velocity < 0:
+        velocity_string+=b'G'
+    velocity_string+=bytes([(int)(255*left_velocity)])
+    if right_velocity > 0:
+        velocity_string+=b'R'
+    elif right_velocity < 0:
+        velocity_string+=b'B'
+    velocity_string+=bytes([(int)(255*right_velocity)])
+    velocity_string = '#@'+str(lv) + ',' + str(rv)+'@#'
+
+    # Publish as ROS message
+    rospy.loginfo(velocity_string)
+    pub.publish(velocity_string)
+
+    # Send as serial output
+    serial_port.write(velocity_string.encode())
+
+    rate.sleep()
+    time.sleep(0.1)
+# Close serial port on shutdown
+serial_port.close()
